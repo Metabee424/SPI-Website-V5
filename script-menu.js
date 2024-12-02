b55a0ffb390bc3ca4f6deec9340e1f2abbf14bf1
Elex <150398711+Metabee424@users.noreply.github.com>
2024-12-01 21:32:11 -0400

    Add files via upload

---
+++ script-menu.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('button-menu');
+    const navWrapper = document.getElementById('nav');
+
+    toggleButton.addEventListener('click', () => {
+        toggleButton.classList.toggle('close');
+        navWrapper.classList.toggle('show');
+    });
+
+    navWrapper.addEventListener('click', (e) => {
+        if (e.target.id === 'nav') {
+            navWrapper.classList.remove('show');
+            toggleButton.classList.remove('close');
+        }
+    });
+});
